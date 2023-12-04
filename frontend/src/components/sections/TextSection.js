152865764007d8b9b35215547e36f7e726f09976
ndri <5224431+ndri@users.noreply.github.com>
2023-12-04 23:00:14 +0100

    Don't display empty text section
    Important for cause areas where we don't have a lot of content yet.

--- frontend/src/components/sections/TextSection.js
+++ frontend/src/components/sections/TextSection.js
@@ -1,9 +1,11 @@
 import Markdown from "../elements/Markdown";
 
 export default function TextSection({ text }) {
+  if (!text) return null;
+
   return (
     <section className="flex justify-center px-4 py-24">
-      <Markdown className="w-full max-w-3xl prose prose-primary">
+      <Markdown className="prose prose-primary w-full max-w-3xl">
         {text}
       </Markdown>
     </section>
