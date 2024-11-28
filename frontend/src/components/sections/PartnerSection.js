82405afa5b8631f570363d9fedc614bed419ed08
ndri <5224431+ndri@users.noreply.github.com>
2024-11-28 15:21:54 +0100

    Fix partner section class attribute

--- frontend/src/components/sections/PartnerSection.js
+++ frontend/src/components/sections/PartnerSection.js
@@ -5,7 +5,7 @@ export default function PartnerSection({ text, image }) {
   return (
     <section className="bg-slate-100 px-4 py-12 md:px-8">
       <div className="mx-auto flex flex-col items-center justify-center gap-4 text-center sm:flex-row sm:gap-10 sm:text-left lg:max-w-2xl">
-        <div class="shrink-0 rounded-full bg-white p-4 shadow-md shadow-slate-200">
+        <div className="shrink-0 rounded-full bg-white p-4 shadow-md shadow-slate-200">
           <Image data={image} className="h-32 w-32" />
         </div>
         <Markdown className="prose prose-primary">{text}</Markdown>
